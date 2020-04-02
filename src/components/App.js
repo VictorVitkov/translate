a5e6f5512e03ca7d72b5c1da86401ca94748239d
VictorVitkov <VictorVitkov@genielifesciences.com>
2020-04-02 08:43:08 -0600

    replacing redux with context

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,19 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
-  state = { language: 'english' };
-
-  onLanguageChange = language => {
-    this.setState({ language })
-  }
-
   render() {
     return (
       <div className="ui container">
-        <div>
-          <h4>Select a Language:</h4>
-          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
-          <i className="flag es" onClick={() => this.onLanguageChange('spanish')} />
-          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
-          <i className="flag bg" onClick={() => this.onLanguageChange('bulgarian')} />
-          <i className="flag kp" onClick={() => this.onLanguageChange('korean')} />
-          <i className="flag ru" onClick={() => this.onLanguageChange('russian')} />
-          <i className="flag cn" onClick={() => this.onLanguageChange('chinese')} />
-        </div>
-        <ColorContext.Provider value="red">
-          <LanguageContext.Provider value={this.state.language}>
-            <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="red">
+              <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     )
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,28 +2,37 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
+
 class Button extends React.Component {
+  renderSubmit(language) {
+    return (
+      language === 'english' ? 'Submit' : // if
+      language === 'dutch' ? 'Voorleggen' : // else if
+      language === 'bulgarian' ? 'Търсене' : // else if
+      language === 'korean' ? '검색' : // else if
+      language === 'russian' ? 'Поиск' : // else if
+      language === 'chinese' ? '搜尋' : // else if
+      language === 'spanish' ? 'Buscar' : // else if
+      null // else
+    )
+  }
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {({ language }) => this.renderSubmit(language)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
   render() {
     return (
       <ColorContext.Consumer>
-        {(color) =>
-          <button className={`ui button ${color}`}>
-            <LanguageContext.Consumer>
-              {(value) =>
-                value === 'english' ? 'Submit' : // if
-                value === 'dutch' ? 'Voorleggen' : // else if
-                value === 'bulgarian' ? 'Търсене' : // else if
-                value === 'korean' ? '검색' : // else if
-                value === 'russian' ? 'Поиск' : // else if
-                value === 'chinese' ? '搜尋' : // else if
-                value === 'spanish' ? 'Buscar' : // else if
-                null // else
-              }
-            </LanguageContext.Consumer>
-          </button>
-        }
+        {color => this.renderButton(color)}
       </ColorContext.Consumer>
-    )
+    );
   }
 }
 
